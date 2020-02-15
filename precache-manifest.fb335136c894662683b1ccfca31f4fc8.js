4034ca1ca958e3c3e4f1f3c05d4604dd23550564
Anishsys <anishnagar61@gmail.com>
2020-02-15 13:45:59 +0530

    Updates

---
+++ precache-manifest.fb335136c894662683b1ccfca31f4fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880227860790fbeffe0f4fb5c893b824",
+    "url": "/shoppingcartdemo/index.html"
+  },
+  {
+    "revision": "c34ac3005bee4b8d9fd7",
+    "url": "/shoppingcartdemo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "95fdaa09f98e22c0f32d",
+    "url": "/shoppingcartdemo/static/js/2.7542bcc0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/shoppingcartdemo/static/js/2.7542bcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34ac3005bee4b8d9fd7",
+    "url": "/shoppingcartdemo/static/js/main.33eeb2dd.chunk.js"
+  },
+  {
+    "revision": "e38808b5cf029b9fc9f6",
+    "url": "/shoppingcartdemo/static/js/runtime-main.5321ec6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppingcartdemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
